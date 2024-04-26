75e8d014f6e01af3ebc81aef171f915427834ecd
Matthias Faust <faust@tropos.de>
2024-04-26 14:59:15 +0200

    Geo class
    The new class Geo contains functions for the rotated coordinate system.

--- soil_init.py
+++ soil_init.py
@@ -128,6 +128,91 @@ class Domain:
         # return xgeomin, xgeomax, ygeomin, ygeomax
         return elongeo, elatgeo
 
+
+class Geo:
+    def __init__(self, domain):
+        self.domain = domain
+
+    def rot2geo(self, xin, yin):
+
+        pollon = self.domain.pol_lon
+        pollat = self.domain.pol_lat
+
+        xgeo = [i for i in xin]
+        ygeo = [i for i in yin]
+
+        pi_r = np.pi/180.
+
+        for i in range(len(xin)):
+            ygeo[i] = 1./pi_r * np.arcsin(
+                np.sin(pi_r*yin[i])*np.sin(pi_r*pollat) +
+                np.cos(pi_r*yin[i])*np.cos(pi_r*xin[i]) *
+                np.cos(pi_r*pollat))
+
+            xgeo[i] = 1./pi_r * np.arctan(
+                (np.cos(pi_r*yin[i])*np.sin(pi_r*xin[i])) /
+                (np.sin(pi_r*pollat)*np.cos(pi_r*yin[i]) *
+                 np.cos(pi_r*xin[i])-np.sin(pi_r*yin[i]) *
+                 np.cos(pi_r*pollat))) + pollon + 180.
+
+            if xgeo[i] > 180.:
+                xgeo[i] -= 360.
+
+        return (xgeo, ygeo)
+
+    def geo2rot(self, xin, yin):
+
+        singlepoint = None
+
+        pollon = self.domain.pol_lon
+        pollat = self.domain.pol_lat
+
+        if type(xin) is float or type(xin) is int:
+            singlepoint = True
+            xin = [xin]
+            yin = [yin]
+
+        xrot = [None] * len(xin)
+        yrot = [None] * len(yin)
+
+        pi_r = np.pi/180.
+
+        for i in range(len(xin)):
+
+            yrot[i] = 1./pi_r * np.arcsin(
+                np.sin(pi_r*yin[i])*np.sin(pi_r*pollat) +
+                np.cos(pi_r*yin[i])*np.cos(pi_r*pollat) *
+                np.cos(pi_r*(xin[i]-pollon)))
+
+            xrot[i] = 1./pi_r * np.arctan(
+                (np.cos(pi_r*yin[i])*np.sin(pi_r*(xin[i]-pollon))) /
+                (np.cos(pi_r*yin[i])*np.sin(pi_r*pollat) *
+                 np.cos(pi_r*(xin[i]-pollon)) -
+                 np.sin(pi_r*yin[i])*np.cos(pi_r*pollat)))
+
+        if singlepoint:
+            return xrot[0], yrot[0]
+        else:
+            return xrot, yrot
+
+    def get_corners(self, rlon, rlat):
+
+        dx = self.domain.dx
+
+        corner_x = [rlon - dx/2,
+                    rlon + dx/2,
+                    rlon + dx/2,
+                    rlon - dx/2]
+        corner_y = [rlat - dx/2,
+                    rlat - dx/2,
+                    rlat + dx/2,
+                    rlat + dx/2]
+
+        corner_x, corner_y = self.rot2geo(corner_x, corner_y)
+
+        return corner_x, corner_y
+
+
 class Soilgrids:
     def __init__(self):
         self.sandfile = None
@@ -189,10 +274,13 @@ class Soilgrids:
 
 def main():
     domain = Domain()
+    soilgrids = Soilgrids()
+    geo = Geo(domain)
+
     domain.read_domain_file()
+    domain.get_rlons_rlats()
     print(domain.sg_res)
 
-    soilgrids = Soilgrids()
     soilgrids.initialize(domain.sg_res)
 
 
